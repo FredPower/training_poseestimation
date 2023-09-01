589cb7f5f05004405f175637d5818b1e4faab329
fredrik <fbiten@kth.se>
2023-09-01 15:43:09 +0200

    change color when correct angle

--- test.py
+++ test.py
@@ -19,6 +19,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 cap = cv2.VideoCapture('video3.mp4')
+angle=0
 while cap.isOpened():
      # read frame
      _, frame = cap.read()
@@ -27,11 +28,13 @@ while cap.isOpened():
          frame = cv2.resize(frame, (600, 600))
          # convert to RGB
          frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+         #Angle display
+         angle_display = (0,0,255)
          
          # process the frame for pose detection
          pose_results = pose.process(frame_rgb)
          #print(pose_results.pose_landmark)
-         angle=0
+         
          #
          try:
               landmarks = pose_results.pose_landmarks.landmark
@@ -44,7 +47,10 @@ while cap.isOpened():
          #
          # draw skeleton on the frame
          mp_drawing.draw_landmarks(frame, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS, mp_drawing.DrawingSpec(color=(255,0,0),thickness=2, circle_radius=2), mp_drawing.DrawingSpec(color=(0,255,0),thickness=2, circle_radius=2))
-         cv2.putText(frame,str(angle),(50,50),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1,(255,0,255),2,cv2.LINE_4)
+
+         if angle < 120:
+              angle_display = (0,255,0)
+         cv2.putText(frame,str(int(angle)),(50,50),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1,angle_display,2,cv2.LINE_4)
          cv2.imshow('Output', frame)
      except:
          print("error-ended")
